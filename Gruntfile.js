72d4a51f627f79277083e53d1f9eb43f50d1db1e
Baptiste Meurant <baptiste@meurant.io>
2017-02-16 10:17:08 +0100

    doc(intro): Initialize intro, usages, languages

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
 			core: {
 				files: {
 					'css/reveal.css': 'css/reveal.scss',
+					'css/custom.css': 'css/custom.scss'
 				}
 			},
 			themes: {
@@ -126,7 +127,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss', 'css/custom.scss' ],
 				tasks: 'css-core'
 			},
 			html: {
